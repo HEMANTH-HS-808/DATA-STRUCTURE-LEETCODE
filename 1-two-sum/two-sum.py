21a19d55d80f205a8ac99913888e912d00bb3562
Hemanth HS <hemanthpushpa9731@gmail.com>
2026-08-04 17:02:53 +0530

    Time: 2125 ms (32.61%) | Memory: 13.4 MB (16.49%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,6 +1,7 @@
-class Solution:
+class Solution(object):
     def twoSum(self, nums, target):
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
+        n=len(nums)
+        for i in range(n):
+            for j in range(i+1,n):
                 if nums[i] + nums[j] == target:
-                    return [i, j]
+                    return[i,j]
